9188828839be4e3d3daf0022b73ed8e8e89ee8e7
wetlo <genericgithub@tutanota.com>
2021-05-16 14:03:20 +0200

    added tokens for images and also paths

--- src/parser/tokens.rs
+++ src/parser/tokens.rs
@@ -1,6 +1,14 @@
+use std::path::PathBuf;
+
 #[derive(Debug)]
 pub enum Token {
     Linefeed,
+    Exclamation,
+    SqrBracketLeft,
+    SqrBracketRight,
+    RoundBracketLeft,
+    RoundBracketRight,
+    Path(PathBuf),
     Text(String),
     Identifier(String),
     ListPre(u8),
