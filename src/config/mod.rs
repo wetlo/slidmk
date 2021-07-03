d6f8d3fa56f2e10d2ce25045a38e5c1d5077f93d
--unset <--unset>
2021-07-03 12:18:25 +0200

    add a basic config

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,1 +1,33 @@
-pub struct Config {}
+use std::collections::HashMap;
+pub type StyleMap = HashMap<String, SlideStyle>;
+pub type Decorations = Vec<(Rectange, usize)>;
+
+pub struct Config<'a> {
+    pub doc_name: &'a str,
+    pub colors: Vec<Color>,
+    pub fg_idx: usize,
+    pub bg_idx: usize,
+    pub slide_styles: StyleMap,
+}
+
+pub struct SlideStyle {
+    /// a decoration for the slides
+    /// draws a simple rectangle at the given position(item0) with the color from the index
+    decorations: Decorations,
+    /// an area were content can appear
+    content: Vec<Rectange>,
+}
+
+/// color struct with rgba values
+/// (red, green, blue, alpha)
+pub struct Color(pub f32, pub f32, pub f32, pub f32);
+
+pub struct Rectange {
+    /// original point from the top-left
+    orig: Point,
+    /// the size of the rectangle relative to the orig Point
+    size: Point,
+}
+
+/// a simple representation of an Rectange with 2 points
+pub struct Point(u32, u32);
