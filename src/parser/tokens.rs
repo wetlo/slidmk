4bd87611442153002514a34caec8633e7d21bb62
wetlo <genericgithub@tutanota.com>
2021-08-27 19:45:58 +0200

    create regexes for tokens

--- src/parser/tokens.rs
+++ src/parser/tokens.rs
@@ -1,3 +1,5 @@
+use super::lexer;
+use regex::{Captures, Regex};
 use std::path::PathBuf;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -10,4 +12,42 @@ pub enum Token {
     Text(String),
     Identifier(String),
     ListPre(u8),
+}
+
+fn regex(re: &str) -> Regex {
+    Regex::new(re).unwrap()
+}
+
+lazy_static::lazy_static! {
+    pub static ref COMMENT: Regex = regex(r"\s*;.*");
+    pub static ref WHITESPACE: Regex = regex(r"\s*");
+
+    pub static ref NON_CAPTURES: [(Regex, Token); 3] = [
+        (regex("["), Token::SqrBracketLeft),
+        (regex("]"), Token::SqrBracketRight),
+        (regex("\n"), Token::Linefeed),
+    ];
+
+    pub static ref CAPTURES: [(Regex, &'static lexer::TokenCreator<Token>); 4] = [
+        (regex(".*"), &text),
+        (regex(r"---\s*(![\s\d]+)"), &identifier),
+        (regex(r"-|\*"), &list_item),
+        (regex(r#""(.*)""#), &path),
+    ];
+}
+
+fn text(_: usize, capture: Captures) -> Token {
+    Token::Text(capture.get(0).unwrap().as_str().to_string())
+}
+
+fn path(_: usize, capture: Captures) -> Token {
+    Token::Path(capture.get(1).unwrap().as_str().into())
+}
+
+fn identifier(_: usize, capture: Captures) -> Token {
+    Token::Identifier(capture.get(1).unwrap().as_str().to_string())
+}
+
+fn list_item(ident: usize, _: Captures) -> Token {
+    Token::ListPre(ident as u8)
 }
